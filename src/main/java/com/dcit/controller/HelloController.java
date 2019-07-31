a82c049f0ffd4e52d64dd6b0e58f7f8c6b5a9709
lihuanyu-0314 <690779233@qq.com>
2019-07-31 15:44:32 +0800

    tes

---
+++ src/main/java/com/dcit/controller/HelloController.java
@@ -0,0 +1,21 @@
+package com.dcit.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Created by lhy on 2019/7/31.
+ */
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello";
+    }
+
+
+}
